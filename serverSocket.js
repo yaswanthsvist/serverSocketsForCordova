faa4e5eadec709a7260d30bb1199572ee6774f88
Yaswanth <yaswanthsvist@gmail.com>
2015-12-01 19:33:47 +0530

    Update serverSocket.js

--- serverSocket.js
+++ serverSocket.js
@@ -1,9 +1,11 @@
 var exec = require('cordova/exec');
 var CORDOVA_SERVICE_NAME = "serverSocketsForCordova";
-var serverSocket=function(){};
-socket.prototype.open=function(succ,fail){
-  succ = success || function() { };
+function serverSocket(){
+	this.onClose = null;
+};
+serverSocket.prototype.open=function(succ,fail){
+  succ = succ|| function() { };
     fail = fail || function() { };
-  exec(succ,fial,CORDOVA_SERVICE_NAME,"open");
+  exec(succ,fail,CORDOVA_SERVICE_NAME,"open",[]);
 }
 module.exports = serverSocket;
